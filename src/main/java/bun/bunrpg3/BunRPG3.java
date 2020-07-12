dc51934053f61d1ddbbd5fca7df1ddfdad5a2f20
Raymond Tran <traymondbiz@gmail.com>
2020-07-11 22:34:21 -0600

    Add useless comment to main class

--- src/main/java/bun/bunrpg3/BunRPG3.java
+++ src/main/java/bun/bunrpg3/BunRPG3.java
@@ -4,6 +4,8 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public final class BunRPG3 extends JavaPlugin {
 
+    // Hello World!
+
     @Override
     public void onEnable() {
         // Plugin startup logic
