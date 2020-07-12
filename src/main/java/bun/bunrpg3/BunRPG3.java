f13dbc94c1e457297a1c7994304ff55c3a35fb5a
Bun Bun <brianaprilgaming@gmail.com>
2020-07-12 00:26:26 -0400

    Initial Commit

---
+++ src/main/java/bun/bunrpg3/BunRPG3.java
@@ -0,0 +1,17 @@
+package bun.bunrpg3;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class BunRPG3 extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
